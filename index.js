c3b7c5554c4e1b5ff2e1d186fa559bc0b76042c5
afazad <abulfajle405@gmail.com>
2022-07-31 01:23:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// console.log("Average of a and b is ", (a+b)/2);
+// console.log("Average of a and b is ", (b+c)/2);
+// console.log("Average of a and b is ", (c+a)/2);
+
+
+function greet(name, greetText){
+    console.log(greetText + name);
+    console.log(name +" is a good boy");
+}
+let name1 = "Harish";
+let name2 = "Raaz";
+let name3 = "ravi";
+let name4= "Christofer";
+let greetText = "Good Morning ";
+
+greet(name1, greetText);
+greet(name2, greetText);
+greet(name3, greetText);
+greet(name4, greetText);
